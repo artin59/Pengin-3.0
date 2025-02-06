eb0310f5b0159c2f84be559079656956d162332a
Artin Kiany <artinkiany59@gmail.com>
2025-02-05 20:07:16 -0500

    login not working :(

--- Headspace/src/main.jsx
+++ Headspace/src/main.jsx
@@ -4,19 +4,20 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 import App from './App.jsx'
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENTID;
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;  
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
-    >
-      <App />
-    </Auth0Provider>,
-  </StrictMode>,
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+     <App />
+    </Auth0Provider>
+  </StrictMode>
 )
