359915505dbf7641ca557288c8727843373e2bb4
Artin Kiany <artinkiany59@gmail.com>
2025-02-07 23:41:14 -0500

    added doctor notes

--- Headspace/Pages/Doctor.tsx
+++ Headspace/Pages/Doctor.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
-
+import Player from "./Player"
+import {useState} from 'react'
+import "./Doctor.css"
 const Doctor = () => {
+
+  const [show, setShow] = useState(false);
+  const [val, setValue] = useState('');
+
+  const handleNote = () => {
+    console.log(val);
+  }
+
+
+  const showPlayer = () => {
+    if (show)
+      setShow(false);
+    else
+      setShow(true);
+  }
+
   return (
-    <div>Doctor</div>
+    <div className="Doctor">
+      <h1>DOCTOR</h1>
+      <h2>Pick a player</h2>
+      <button className="playerNameBtn" onClick={showPlayer}> L9 Ligma </button>
+      {show && (<Player/>)}
+
+      <p>Notes</p>
+      <textarea value={val} rows={4} cols={50} onChange={(e)=>setValue(e.target.value)}></textarea>
+      <button className="submitBtn" onClick={handleNote}>Submit</button>
+
+    </div>
   )
 }
 
