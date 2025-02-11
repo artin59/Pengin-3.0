bc63bdeb535e724049ab47fe7761897508d386cf
Artin Kiany <artinkiany59@gmail.com>
2025-02-11 16:13:16 -0500

    did the same as last commit but for the doctor page

--- Headspace/src/Pages/Doctor.tsx
+++ Headspace/src/Pages/Doctor.tsx
@@ -6,6 +6,7 @@ const Doctor = () => {
 
   const [show, setShow] = useState(false);
   const [note, setNote] = useState('');
+  const [showButton, setButton] = useState(false);
 
   const {setItem} = useLocalStorage('note');
 
@@ -16,23 +17,38 @@ const Doctor = () => {
       setShow(true);
   }
 
+  const showDoctorButton = () => {
+    if (showButton)
+      setButton(false);
+    else
+      setButton(true);
+  }
+
+  const showBoth = () => {
+    showPlayer();
+    showDoctorButton();
+  }
 
   const callBoth = () => {
     setItem(note);
     setNote("");
+    alert("You have Submitted the note!");
   }
 
   return (
     <div className="Doctor">
       <h1>DOCTOR</h1>
       <h2>Pick a player</h2>
-      <button className="playerNameBtn" onClick={showPlayer}> L9 Ligma </button>
+      <button className="playerNameBtn" onClick={showBoth}> L9 Ligma </button>
       {show && (<Player/>)}
 
-      <p>Notes</p>
+      {showButton && 
+
+      <div className="DoctorsNoteSubmit">
+      <p>Notes:</p>
       <textarea value={note} rows={4} cols={50} onChange={(e)=>setNote(e.target.value)}></textarea>
       <button className="submitBtn" onClick={callBoth}>Submit</button>
-
+      </div>}
     </div>
   )
 }
