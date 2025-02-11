20010174702274a4c2eeff3fa64fd56fce28a533
Artin Kiany <artinkiany59@gmail.com>
2025-02-11 16:00:53 -0500

    player button shows both player page and also the doctors note button

--- Headspace/src/Pages/Coach.tsx
+++ Headspace/src/Pages/Coach.tsx
@@ -8,6 +8,7 @@ const Coach = () => {
 
   const [show, setShow] = useState(false);
   const [note, setNote] = useState('');
+  const [showButton, setButton] = useState(false);
 
   const {getItem} = useLocalStorage("note");
 
@@ -19,10 +20,20 @@ const Coach = () => {
   }
 
   const showDoctorNote = () => {
-    
     const savedNote = getItem();
     setNote(savedNote || "No doctor's note found")
+  }
 
+  const showDoctorButton = () => {
+    if (showButton)
+      setButton(false);
+    else
+      setButton(true);
+  }
+
+  const showBoth = () => {
+    showPlayer();
+    showDoctorButton();
   }
 
   return (
@@ -31,14 +42,17 @@ const Coach = () => {
 
       <h1>Coach</h1>
       <h2>Pick a player</h2>
-      <button className="playerNameBtn" onClick={showPlayer}> L9 Ligma </button>
+      <button className="playerNameBtn" onClick={showBoth}> L9 Ligma </button>
       {show && (<Player/>)}
-      <button className="submitBtn" onClick={showDoctorNote}>Get Doctor's Note</button>
-      {note && 
-      <div className="DoctorsNote">
-        <p>{note}</p>
-      </div>}
 
+      {showButton && 
+      <div>
+        <button className="submitBtn" onClick={showDoctorNote}>Get Doctor's Note</button>
+        {note && 
+        <div className="DoctorsNote">
+          <p>{note}</p>
+        </div>}
+      </div>}
     </div>
 
   )
